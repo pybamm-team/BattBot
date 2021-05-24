34fa92459e2d523acf4ba29e3233ab41a80108c8
Saransh <saransh0701@gmail.com>
2021-05-24 17:45:38 +0530

    Update tests  and coverage

--- test/test_plot_graph.py
+++ test/test_plot_graph.py
@@ -11,15 +11,22 @@ class TestPlotting(unittest.TestCase):
     def setUp(self):
         self.model = pybamm.lithium_ion.DFN()
         self.parameter_values = self.model.default_parameter_values
+        self.sim = pybamm.Simulation(self.model, parameter_values=self.parameter_values)
 
     def tearDown(self):
         os.remove('plot.png')
 
-    def test_plot_graph(self):
-        sim = pybamm.Simulation(self.model, parameter_values=self.parameter_values)
-        sim.solve([0, 3600])
-        solution = sim.solution
-        plot_graph(solution=solution, sim=sim)
+    def test_plot_graph_with_solution(self):
+        self.sim.solve([0, 3700])
+        solution = self.sim.solution
+        plot_graph(solution=solution, sim=self.sim)
+        path = 'plot.png'
+
+        assert os.path.exists(path)
+
+    def test_plot_graph_with_simulation(self):
+        self.sim.solve([0, 3700])
+        plot_graph(sim=self.sim)
         path = 'plot.png'
 
         assert os.path.exists(path)
