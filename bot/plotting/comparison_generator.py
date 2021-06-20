85402e7af09a10cb8106e6e53ed37e705bdc5336
Saransh <saransh0701@gmail.com>
2021-06-21 02:26:10 +0530

    Refactor comparison_generator

--- bot/plotting/comparison_generator.py
+++ bot/plotting/comparison_generator.py
@@ -29,174 +29,87 @@ def comparison_generator(
             list(enumerate(param_list))
         )
 
-        choice = random.randint(0, 1)
-        if provided_choice is not None:
-            choice = provided_choice
+    choice = random.randint(0, 1)
+    if provided_choice is not None:
+        choice = provided_choice
 
-        if choice == 0:
-            s = pybamm.BatchStudy(
-                models=models_for_comp,
-                parameter_values=parameter_values_for_comp,
-                permutations=True,
-            )
+    if choice == 0:
+        s = pybamm.BatchStudy(
+            models=models_for_comp,
+            parameter_values=parameter_values_for_comp,
+            permutations=True,
+        )
 
-            s.solve([0, 3700])
+        s.solve([0, 3700])
 
-            time_array = plot_graph(sim=s.sims)
+        time_array = plot_graph(sim=s.sims)
 
-            comparison_dict["model"] = models_for_comp
-            comparison_dict["parameter_values"] = params
-            comparison_dict["time_array"] = time_array
-            comparison_dict["chemistry"] = chemistry
+        comparison_dict["model"] = models_for_comp
+        comparison_dict["parameter_values"] = params
+        comparison_dict["time_array"] = time_array
+        comparison_dict["chemistry"] = chemistry
 
-            return comparison_dict
+        return comparison_dict
 
-        elif choice == 1:
+    elif choice == 1:
 
-            while True:
-                try:
-                    cycle = experiment_generator()
-                    number = random.randint(1, 3)
+        while True:
+            try:
+                cycle = experiment_generator()
+                number = random.randint(1, 3)
 
-                    if provided_choice is not None:
-                        experiment = [
-                            (
-                                "Discharge at C/10 for 10 hours "
-                                + "or until 3.3 V",
-                                "Rest for 1 hour",
-                                "Charge at 1 A until 4.1 V",
-                                "Hold at 4.1 V until 50 mA",
-                                "Rest for 1 hour"
-                            )
-                        ]
-                        number = 1
+                if provided_choice is not None:
+                    experiment = [
+                        (
+                            "Discharge at C/10 for 10 hours "
+                            + "or until 3.3 V",
+                            "Rest for 1 hour",
+                            "Charge at 1 A until 4.1 V",
+                            "Hold at 4.1 V until 50 mA",
+                            "Rest for 1 hour"
+                        )
+                    ]
+                    number = 1
 
-                    experiment = dict(
-                        list(
-                            enumerate(
-                                [
-                                    pybamm.Experiment(
-                                        cycle * number
-                                    )
-                                ]
-                            )
+                experiment = dict(
+                    list(
+                        enumerate(
+                            [
+                                pybamm.Experiment(
+                                    cycle * number
+                                )
+                            ]
                         )
                     )
+                )
 
-                    s = pybamm.BatchStudy(
-                        models=models_for_comp,
-                        parameter_values=parameter_values_for_comp,
-                        experiments=experiment,
-                        permutations=True,
-                    )
+                s = pybamm.BatchStudy(
+                    models=models_for_comp,
+                    parameter_values=parameter_values_for_comp,
+                    experiments=experiment,
+                    permutations=True,
+                )
 
-                    s.solve()
+                s.solve()
 
-                    max_time = 0
-                    solution = s.sims[0].solution
-                    for sim in s.sims:
-                        if sim.solution["Time [s]"].entries[-1] > max_time:
-                            max_time = sim.solution["Time [s]"].entries[-1]
-                            solution = sim.solution
+                max_time = 0
+                solution = s.sims[0].solution
+                for sim in s.sims:
+                    if sim.solution["Time [s]"].entries[-1] > max_time:
+                        max_time = sim.solution["Time [s]"].entries[-1]
+                        solution = sim.solution
 
-                    time_array = plot_graph(
-                        solution=solution,
-                        sim=s.sims
-                    )
+                time_array = plot_graph(
+                    solution=solution,
+                    sim=s.sims
+                )
 
-                    comparison_dict["model"] = models_for_comp
-                    comparison_dict["parameter_values"] = params
-                    comparison_dict["time_array"] = time_array
-                    comparison_dict["chemistry"] = chemistry
+                comparison_dict["model"] = models_for_comp
+                comparison_dict["parameter_values"] = params
+                comparison_dict["time_array"] = time_array
+                comparison_dict["chemistry"] = chemistry
 
-                    return comparison_dict
+                return comparison_dict
 
-                except Exception as e:  # pragma: no cover
-                    print(e)
-
-    else:
-
-        choice = random.randint(0, 1)
-        if provided_choice is not None:
-            choice = provided_choice
-
-        if choice == 0:
-            s = pybamm.BatchStudy(
-                models=models_for_comp,
-                parameter_values=parameter_values_for_comp,
-                permutations=True,
-            )
-
-            s.solve([0, 3700])
-
-            time_array = plot_graph(sim=s.sims)
-
-            comparison_dict["model"] = models_for_comp
-            comparison_dict["parameter_values"] = params
-            comparison_dict["time_array"] = time_array
-            comparison_dict["chemistry"] = chemistry
-
-            return comparison_dict
-
-        elif choice == 1:
-
-            while True:
-                try:
-                    cycle = experiment_generator()
-                    number = random.randint(1, 3)
-
-                    if provided_choice is not None:
-                        experiment = [
-                            (
-                                "Discharge at C/10 for 10 hours "
-                                + "or until 3.3 V",
-                                "Rest for 1 hour",
-                                "Charge at 1 A until 4.1 V",
-                                "Hold at 4.1 V until 50 mA",
-                                "Rest for 1 hour"
-                            )
-                        ]
-                        number = 1
-
-                    experiment = dict(
-                        list(
-                            enumerate(
-                                [
-                                    pybamm.Experiment(
-                                        cycle * number
-                                    )
-                                ]
-                            )
-                        )
-                    )
-
-                    s = pybamm.BatchStudy(
-                        models=models_for_comp,
-                        parameter_values=parameter_values_for_comp,
-                        experiments=experiment,
-                        permutations=True,
-                    )
-
-                    s.solve()
-
-                    max_time = 0
-                    solution = s.sims[0].solution
-                    for sim in s.sims:
-                        if sim.solution["Time [s]"].entries[-1] > max_time:
-                            max_time = sim.solution["Time [s]"].entries[-1]
-                            solution = sim.solution
-
-                    time_array = plot_graph(
-                        solution=solution,
-                        sim=s.sims
-                    )
-
-                    comparison_dict["model"] = models_for_comp
-                    comparison_dict["parameter_values"] = params
-                    comparison_dict["time_array"] = time_array
-                    comparison_dict["chemistry"] = chemistry
-
-                    return comparison_dict
-
-                except Exception as e:  # pragma: no cover
-                    print(e)
+            except Exception as e:  # pragma: no cover
+                print(e)
