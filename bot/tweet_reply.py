1a1ecc4d2c8f9f766ce0ab1ddc30038cb74d248d
Saransh <saransh0701@gmail.com>
2021-07-13 21:04:33 +0530

    More tests

--- bot/tweet_reply.py
+++ bot/tweet_reply.py
@@ -13,6 +13,9 @@ api = tweepy.API(auth)
 # inspired from https://www.youtube.com/watch?v=W0wWwglE1Vc
 class Reply:
 
+    def __init__(self, testing=False):
+        self.testing = testing
+
     def retrieve_tweet_id(self, file_name):
         """
         Returns the id of the tweet (in which the bot was mentioned)
@@ -38,37 +41,41 @@ class Reply:
         Replies to a tweet where the bot was mentioned with the
         hashtag "battbot".
         """
-        last_seen_id = self.retrieve_tweet_id("last_seen_id.txt")
+        if self.testing:
+            last_seen_id = self.retrieve_tweet_id("bot/last_seen_id.txt")
+        else:   # pragma: no cover
+            last_seen_id = self.retrieve_tweet_id("last_seen_id.txt")
 
         # retreiving all the mentions after the tweet with id=last_seen_id
         mentions = api.mentions_timeline(last_seen_id, tweet_mode="extended")
 
-        # iterating through all the mentions
-        for mention in reversed(mentions):
-            # storing the id
-            self.store_tweet_id(mention._json["id"], "last_seen_id.txt")
-            print(
-                mention.user.screen_name + " "
-                + mention.full_text + " "
-                + str(mention._json["id"])
-            )
-
-            # reading the tweet text
-            if '#battbot' in mention.full_text.lower():
-
-                # replying
-                api.update_status(
-                    "@"
-                    + mention.user.screen_name
-                    + " Hi there! The replying feature is still under "
-                    + "active development. "
-                    + "Try this again after a few weeks:)",
-                    mention._json["id"]
+        # iterating through all the mentions if not testing
+        if not self.testing:    # pragma: no cover
+            for mention in reversed(mentions):
+                # storing the id
+                self.store_tweet_id(mention._json["id"], "last_seen_id.txt")
+                print(
+                    mention.user.screen_name + " "
+                    + mention.full_text + " "
+                    + str(mention._json["id"])
                 )
 
+                # reading the tweet text
+                if '#battbot' in mention.full_text.lower():
 
-if __name__ == "__main__":
+                    # replying
+                    api.update_status(
+                        "@"
+                        + mention.user.screen_name
+                        + " Hi there! The replying feature is still under "
+                        + "active development. "
+                        + "Try this again after a few weeks:)",
+                        mention._json["id"]
+                    )
+
+
+if __name__ == "__main__":  # pragma: no cover
+    reply = Reply()
     while True:
-        reply = Reply()
         reply.reply_to_tweet()
         time.sleep(60)
