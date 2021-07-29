92f9c2d6f28d8a73c07ee2e720d60b6ce950f856
Saransh <saransh0701@gmail.com>
2021-07-29 21:08:48 +0530

    fix tests

--- test/without_keys/test_create_gif.py
+++ test/without_keys/test_create_gif.py
@@ -7,13 +7,13 @@ from bot.plotting.create_gif import create_gif
 class TestCreateGIF(unittest.TestCase):
     def setUp(self):
         self.model = {0: pybamm.lithium_ion.DFN()}
-        batch_study = pybamm.BatchStudy(self.model)
+        self.batch_study = pybamm.BatchStudy(self.model)
 
     def tearDown(self):
         os.remove("plot.gif")
 
     def test_create_gif(self):
-        create_gif(batch_study)
+        create_gif(self.batch_study)
         path = "plot.gif"
         assert os.path.exists(path)
 
